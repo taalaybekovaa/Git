7067d2065fc00f7f3dd80e30083c8b3defcdfeb0
Altynai <58652169+taalaybekovaa@users.noreply.github.com>
2022-01-04 19:34:56 -0600

    Fifth commit

--- src/Test.java
+++ src/Test.java
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("Second");
         System.out.println("Third");
         System.out.println("Fourth");
+        System.out.println("Fifth");
     }
 }
