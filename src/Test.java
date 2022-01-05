fa9bedb450d7b48bde1246817232f83e7d5611c2
Altynai <58652169+taalaybekovaa@users.noreply.github.com>
2022-01-04 18:43:09 -0600

    Third commit

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Second");
+        System.out.println("Third");
+        System.out.println("Fourth");
     }
 }
